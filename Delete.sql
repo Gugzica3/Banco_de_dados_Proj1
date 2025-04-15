5328d13a8a93bf41d9b4298f7e97d304e119385f
Gustavo Bertoluzzi <126602869+Gugzica3@users.noreply.github.com>
2025-04-15 14:46:52 -0300

    Create Delete.sql

---
+++ Delete.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS
+  HistoricoEscolar,
+  Nota,
+  Avaliacao,
+  Aula,
+  Aluno,
+  Professor,
+  Grupo_TCC,
+  TCC,
+  Turma,
+  Disciplina,
+  Curso,
+  Depto,
+  Pessoa
+CASCADE;
