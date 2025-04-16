518d3fddcc2d853287af029477b47d0636284fff
Gustavo Bertoluzzi <126602869+Gugzica3@users.noreply.github.com>
2025-04-16 16:14:33 -0300

    Update Querie_2.sql

--- Queries/Querie_2.sql
+++ Queries/Querie_2.sql
@@ -1,16 +1,29 @@
 SELECT
     t.id_grupo,
-    t.titulo,
-    p_orient.nome                        AS professor_orientador,
-    p_aluno.nome                         AS aluno_integrante
-FROM TCC                t
-JOIN Professor      prof ON prof.id_professor = t.orientador
-JOIN Pessoa      p_orient ON p_orient.cpf     = prof.cpf
-JOIN Grupo_TCC           g ON g.id_grupo     = t.id_grupo
-LEFT JOIN LATERAL (
-        VALUES (g.aluno1), (g.aluno2), (g.aluno3), (g.aluno4), (g.aluno5)
-    ) AS g_unpivot(id_aluno)            ON TRUE
-JOIN Aluno               a ON a.id_aluno     = g_unpivot.id_aluno
-JOIN Pessoa          p_aluno ON p_aluno.cpf  = a.cpf
-WHERE t.orientador = 6
-ORDER BY t.id_grupo, p_aluno.nome;
+    t.titulo                                  AS titulo_tcc,
+    orient.nome                               AS professor_orientador,
+    aluno_pessoa.nome                         AS nome_aluno
+FROM TCC t
+
+JOIN Professor       pr    ON pr.id_professor = t.orientador
+JOIN Pessoa          orient ON orient.cpf    = pr.cpf
+
+
+JOIN Grupo_TCC       g     ON g.id_grupo      = t.id_grupo
+
+
+CROSS JOIN LATERAL (
+    VALUES
+      (g.aluno1),
+      (g.aluno2),
+      (g.aluno3),
+      (g.aluno4),
+      (g.aluno5)
+) AS membros(id_aluno)
+
+JOIN Aluno           a     ON a.id_aluno      = membros.id_aluno
+JOIN Pessoa          aluno_pessoa ON aluno_pessoa.cpf = a.cpf
+
+ORDER BY
+    t.id_grupo,
+    aluno_pessoa.nome;
