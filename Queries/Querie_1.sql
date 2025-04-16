3e7831a0a133c1c630bf000dfd8c8f89d5f86c2f
Gustavo Bertoluzzi <126602869+Gugzica3@users.noreply.github.com>
2025-04-16 15:06:42 -0300

    Update Querie_1.sql

--- Queries/Querie_1.sql
+++ Queries/Querie_1.sql
@@ -1,38 +1,39 @@
-SELECT 
-    p.nome AS aluno,
-    d.nome AS disciplina,
-    t_fail.ano AS ano_reprovacao,
-    t_fail.semestre_ano AS semestre_reprovacao,
-    av_fail.nota AS nota_reprovacao,
-    t_pass.ano AS ano_aprovacao,
-    t_pass.semestre_ano AS semestre_aprovacao,
-    av_pass.nota AS nota_aprovacao
-FROM HistoricoEscolar he_fail
-JOIN Nota n_fail 
-  ON he_fail.id_nota = n_fail.id_nota
-JOIN Avaliacao av_fail 
-  ON n_fail.id_avaliacao = av_fail.id_avaliacao
-JOIN Turma t_fail 
-  ON he_fail.id_turma = t_fail.id_turma
-JOIN HistoricoEscolar he_pass 
-  ON he_fail.id_aluno = he_pass.id_aluno
-JOIN Nota n_pass 
-  ON he_pass.id_nota = n_pass.id_nota
-JOIN Avaliacao av_pass 
-  ON n_pass.id_avaliacao = av_pass.id_avaliacao
-JOIN Turma t_pass 
-  ON he_pass.id_turma = t_pass.id_turma
-JOIN Aluno a 
-  ON he_fail.id_aluno = a.id_aluno
-JOIN Pessoa p 
-  ON a.cpf = p.cpf
-JOIN Disciplina d 
-  ON t_fail.id_disciplina = d.id_disciplina
-WHERE he_fail.id_aluno = 1
-  AND t_fail.id_disciplina = t_pass.id_disciplina
-  AND av_fail.nota < 6
-  AND av_pass.nota >= 6
-  AND (t_pass.ano > t_fail.ano 
-       OR (t_pass.ano = t_fail.ano 
-           AND t_pass.semestre_ano > t_fail.semestre_ano))
-ORDER BY t_fail.ano, t_fail.semestre_ano;
+WITH dados AS (
+    SELECT
+        he.id_aluno,
+        pes.nome                 AS aluno,
+        d.id_disciplina,
+        d.nome                   AS disciplina,
+        t.ano,
+        t.semestre_ano::INT      AS semestre,
+        a.nota,
+        ROW_NUMBER() OVER (
+            PARTITION BY he.id_aluno, d.id_disciplina
+            ORDER BY a.id_avaliacao           
+        ) AS tentativa_ord
+    FROM HistoricoEscolar he
+    JOIN Nota       n   ON n.id_nota       = he.id_nota
+    JOIN Avaliacao  a   ON a.id_avaliacao  = n.id_avaliacao
+    JOIN Turma      t   ON t.id_turma      = he.id_turma
+    JOIN Disciplina d   ON d.id_disciplina = t.id_disciplina
+    JOIN Aluno      al  ON al.id_aluno     = he.id_aluno
+    JOIN Pessoa     pes ON pes.cpf         = al.cpf
+),
+aluno_caso AS (        
+    SELECT DISTINCT
+           d1.id_aluno,
+           d1.id_disciplina
+    FROM dados d1
+    JOIN dados d2
+      ON d2.id_aluno      = d1.id_aluno
+     AND d2.id_disciplina = d1.id_disciplina
+     AND d2.tentativa_ord > d1.tentativa_ord   
+    WHERE d1.nota < 6                          
+      AND d2.nota >= 6                         
+)
+SELECT d.*
+FROM dados d
+JOIN aluno_caso ac
+  ON ac.id_aluno      = d.id_aluno
+ AND ac.id_disciplina = d.id_disciplina
+ORDER BY d.id_aluno, d.disciplina, d.tentativa_ord;
